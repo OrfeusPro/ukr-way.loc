68ed6212c470d87d494fb7b782e0cd3a94bb16c7
Valentin <61696543+OrfeusPro@users.noreply.github.com>
2022-06-15 16:05:35 +0300

    Update main_forma.blade.php

--- resources/views/way/parts/main_forma.blade.php
+++ resources/views/way/parts/main_forma.blade.php
@@ -6,7 +6,7 @@
                     <div class="row">
                         <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
                             <label class="uk-form-label" for="form-stacked-text">ЗВІДКИ?</label>
-                            <input type="text" id="otkuda" name="otkuda" placeholder="Пункт отправления" list="routes" required>
+                            <input type="text" class="form-control" id="otkuda" name="otkuda" placeholder="Пункт отправления" list="routes" required>
                             <datalist id="routes">
 								@foreach ($dist_r_from as $from)
 									<option value="{{ $from->r_from }}">
@@ -18,7 +18,7 @@
                         </div>
                         <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
                             <label class="uk-form-label" for="form-stacked-text">КУДИ?</label>
-                            <input type="text" id="kuda" name="kuda" placeholder="Пункт прибытия" list="routes2" required>
+                            <input type="text" class="form-control" id="kuda" name="kuda" placeholder="Пункт прибытия" list="routes2" required>
                             <datalist id="routes2">
 								@foreach ($dist_r_to as $to)
 									<option value="{{ $to->r_to }}">
@@ -30,16 +30,16 @@
                         </div>
                         <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
                             <label class="uk-form-label" for="form-stacked-text">ХТО ЇДЕ?</label>
-                            <input type="text" class="" id="name" name="name" placeholder="Имя клиента" required>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Имя клиента" required>
                         </div>
                         <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
                             <label class="uk-form-label" for="form-stacked-text">НОМЕР ТЕЛЕФОНУ</label>
-                            <input type="tel" class="" id="phone" name="phone" placeholder="___ ___ __ __" required>
+                            <input type="tel" class="form-control" id="phone" name="phone" placeholder="___ ___ __ __" required>
                         </div>
 
                         <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
                             <label class="uk-form-label" for="form-stacked-text">ДАТА</label>
-                            <input type="date" name="date" id="nowdate" class="hb-input"  placeholder="__/__/____" required>
+                            <input type="date" name="date" id="nowdate" class="form-control hb-input"  placeholder="__/__/____" required>
                         </div>
 
                         <div class="col-lg-2 col-md-2 col-sm-12 p-0 hb_form">

2614375245a6addbb3acc2ad780751180ed096fc
Valentin <61696543+OrfeusPro@users.noreply.github.com>
2022-06-19 15:29:41 +0300

    Update main_forma.blade.php

--- resources/views/way/parts/main_forma.blade.php
+++ resources/views/way/parts/main_forma.blade.php
@@ -1,58 +1,3 @@
-{{-- <section class="search-sec pt-5 pb-5">
-    <div class="container box-shadow bg-white radius-md">
-        <form action="./complete.php" method="post" id="mainform_action">
-            <div class="row">
-                <div class="col-lg-12 font-14">
-                    <div class="row">
-                        <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
-                            <label class="uk-form-label" for="form-stacked-text">ЗВІДКИ?</label>
-                            <input type="text" class="form-control" id="otkuda" name="otkuda" placeholder="Пункт отправления" list="routes" required>
-                            <datalist id="routes">
-								@foreach ($dist_r_from as $from)
-									<option value="{{ $from->r_from }}">
-								@endforeach
-								@foreach ($dist_r_to as $to)
-									<option value="{{ $to->r_to }}">
-								@endforeach
-                            </datalist>
-                        </div>
-                        <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
-                            <label class="uk-form-label" for="form-stacked-text">КУДИ?</label>
-                            <input type="text" class="form-control" id="kuda" name="kuda" placeholder="Пункт прибытия" list="routes2" required>
-                            <datalist id="routes2">
-								@foreach ($dist_r_to as $to)
-									<option value="{{ $to->r_to }}">
-								@endforeach
-								@foreach ($dist_r_from as $from)
-									<option value="{{ $from->r_from }}">
-								@endforeach
-                            </datalist>
-                        </div>
-                        <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
-                            <label class="uk-form-label" for="form-stacked-text">ХТО ЇДЕ?</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Имя клиента" required>
-                        </div>
-                        <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
-                            <label class="uk-form-label" for="form-stacked-text">НОМЕР ТЕЛЕФОНУ</label>
-                            <input type="tel" class="form-control" id="phone" name="phone" placeholder="___ ___ __ __" required>
-                        </div>
-
-                        <div class="col-lg-2 col-md-2 col-12 p-0 hb_form xs-pt xxs-pt-mobile xxs-pb">
-                            <label class="uk-form-label" for="form-stacked-text">ДАТА</label>
-                            <input type="date" name="date" id="nowdate" class="form-control hb-input"  placeholder="__/__/____" required>
-                        </div>
-
-                        <div class="col-lg-2 col-md-2 col-sm-12 p-0 hb_form">
-                            <button type="submit" class="btn wrn-btn zamir hb_bottom_zabronirovat">ЗАБРОНЮВАТИ</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-    </div>
-</section> --}}
-
-
 <section class="py-0">
 	<div class="xs-py" data-bg-image="way/images/ua.jpg">
 		
@@ -87,7 +32,7 @@
 								@endforeach
 							</datalist>
 						</div>
-						<div class="col-12 col-md-3 xxs-mt-mobile">
+						<div class="col-12 col-md-3">
 							<input class="form-control" type="date" value="{{ date('Y-m-d') }}">
 						</div>
 						<div class="col-12 col-md-3 xxs-mt-mobile">
